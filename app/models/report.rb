1050c9d75adf030843e6e017ab1d784c9863a696
Sourav Moitra <sourav.moitr@gmail.com>
2015-03-02 11:45:39 +0530

    Styling changes part3

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,5 @@
 class Report < ActiveRecord::Base
   belongs_to :reportable, polymorphic: true
+
+  validates :reason, presence: true, length: { maximum: 50 }
 end
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @report = reports(:one)
+  end
+
+  test "it should be valid" do
+    assert @report.valid?
+  end
+
+  test "it reason should be present" do
+    @report.reason = " "
+    assert_not @report.valid?
+  end
+
+  test "it reason should not be too long" do
+    @report.reason = "a" * 51
+    assert_not @report.valid?
+  end
 end
