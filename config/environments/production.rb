39ab54bb620d77445a77a564327b0811113de632
Sourav Moitra <sourav.moitr@gmail.com>
2015-03-05 19:05:43 +0530

    mail setting moved to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,3 +79,12 @@ Rails.application.configure do
 end
 
 Rails.application.routes.default_url_options[:host] = 'sm-awesome-gallery.herokuapp.com'
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
