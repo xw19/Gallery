2adfbead25cf0dd3346b4125f4563db9bf86e605
Sourav Moitra <sourav.moitr@gmail.com>
2015-03-07 18:10:03 +0530

    removed puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,24 +22,24 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-namespace :foreman do
-  desc "Export the Procfile to Ubuntu's upstart scripts"
-  task :export, :roles => :app do
-    run "cd #{current_path} && #{sudo} foreman export upstart /etc/init -a #{app_name} -u #{user} -l /var/#{app_name}/log"
-  end
-
-  desc "Start the application services"
-  task :start, :roles => :app do
-    run "#{sudo} service #{app_name} start"
-  end
-
-  desc "Stop the application services"
-  task :stop, :roles => :app do
-    run "#{sudo} service #{app_name} stop"
-  end
-
-  desc "Restart the application services"
-  task :restart, :roles => :app do
-    run "#{sudo} service #{app_name} start || #{sudo} service #{app_name} restart"
-  end
-end
+# namespace :foreman do
+#   desc "Export the Procfile to Ubuntu's upstart scripts"
+#   task :export, :roles => :app do
+#     run "cd #{current_path} && #{sudo} foreman export upstart /etc/init -a #{app_name} -u #{user} -l /var/#{app_name}/log"
+#   end
+#
+#   desc "Start the application services"
+#   task :start, :roles => :app do
+#     run "#{sudo} service #{app_name} start"
+#   end
+#
+#   desc "Stop the application services"
+#   task :stop, :roles => :app do
+#     run "#{sudo} service #{app_name} stop"
+#   end
+#
+#   desc "Restart the application services"
+#   task :restart, :roles => :app do
+#     run "#{sudo} service #{app_name} start || #{sudo} service #{app_name} restart"
+#   end
+# end
